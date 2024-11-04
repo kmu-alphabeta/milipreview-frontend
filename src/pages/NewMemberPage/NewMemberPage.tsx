6f6aa00c58b9a28eddf658cd6174cf4e7cef3620
pkm021118 <pkm021117@gmail.com>
2024-11-05 05:51:52 +0900

    [fix]NewMemberPage 수정

--- src/pages/NewMemberPage/NewMemberPage.tsx
+++ src/pages/NewMemberPage/NewMemberPage.tsx
@@ -2,18 +2,14 @@
 
 import React from 'react';
 import * as m from './style';
-import Header from '../../componenets/Header/index';
+import Header from '../../componenets/Header';
+import NewMemberForm from '../../componenets/NewMemberForm/NewMemberForm';
 
 const NewMemberPage: React.FC = () => {
   return (
     <m.Container>
       <Header />
-      <m.InnerContainer>
-        <m.MemberBox>
-          <m.Title>신규 회원 페이지</m.Title>
-          <m.Text>신규 회원 정보를 관리하세요.</m.Text>
-        </m.MemberBox>
-      </m.InnerContainer>
+      <NewMemberForm />
     </m.Container>
   );
 };

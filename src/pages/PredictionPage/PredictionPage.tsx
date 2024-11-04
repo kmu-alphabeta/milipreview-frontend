aa367d6281c4360545bda4c3676df1496cdb4915
pkm021118 <pkm021117@gmail.com>
2024-11-05 05:30:35 +0900

    [feat]PredictionPage 생성

---
+++ src/pages/PredictionPage/PredictionPage.tsx
@@ -0,0 +1,21 @@
+// src/pages/PredictionPage/PredictionPage.tsx
+
+import React from 'react';
+import * as m from './style';
+import Header from '../../componenets/Header/index';
+
+const PredictionPage: React.FC = () => {
+  return (
+    <m.Container>
+      <Header />
+      <m.InnerContainer>
+        <m.PredictionBox>
+          <m.Title>예측 페이지</m.Title>
+          <m.Text>여기에서 예측 데이터를 확인하고 관리하세요.</m.Text>
+        </m.PredictionBox>
+      </m.InnerContainer>
+    </m.Container>
+  );
+};
+
+export default PredictionPage;
---
+++ src/pages/PredictionPage/style.tsx
@@ -0,0 +1,44 @@
+// src/pages/PredictionPage/style.tsx
+
+import styled from 'styled-components';
+
+export const Container = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  margin: 0% 16%;
+  margin-bottom: 20px;
+  gap: 15px;
+`;
+
+export const InnerContainer = styled.div`
+  display: flex;
+  align-items: center;
+  gap: 2%;
+  width: 100%;
+`;
+
+export const PredictionBox = styled.div`
+  height: 650px;
+  width: 100%;
+  border: 1px solid #e4e4e4;
+  border-radius: 10px;
+  box-shadow: 0px 2px 4px 0px #e4e4e4;
+  padding: 30px 40px;
+  display: flex;
+  flex-direction: column;
+  gap: 20px;
+  align-items: center;
+`;
+
+export const Title = styled.h1`
+  font-size: 24px;
+  font-weight: bold;
+  color: #333;
+`;
+
+export const Text = styled.p`
+  font-size: 16px;
+  color: #666;
+  text-align: center;
+`;

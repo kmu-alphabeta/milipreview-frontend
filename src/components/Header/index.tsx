7cf0f094c4fcf1ef09043a5ed0b5801753cd3fc7
MJspace <mjsa02@kookmin.ac.kr>
2024-11-05 10:05:18 +0900

    feat: 헤더 컴포넌트 추가

---
+++ src/components/Header/index.tsx
@@ -0,0 +1,29 @@
+import { useLocation } from 'react-router-dom';
+import * as h from './style';
+import Logo from '../../assets/MILI PREVIEW.svg';
+const MainPage: React.FC = () => {
+  const navigate = useLocation();
+  const onClick = () => {
+    if (navigate.pathname === '/') {
+      window.location.reload(); //홈 새로고침
+    } else {
+      window.location.href = '/'; // 홈으로 이동
+    }
+  };
+  return (
+    <h.Container>
+      <img
+        onClick={onClick}
+        src={Logo}
+        alt="Mili Preview Logo"
+        style={{ width: '95px', height: '30px', cursor: 'pointer' }}
+      />
+      <h.ButtonContainer>
+        <h.TextButton>예측 조회 및 관리</h.TextButton>
+        <h.TextButton>Logout</h.TextButton>
+      </h.ButtonContainer>
+    </h.Container>
+  );
+};
+
+export default MainPage;

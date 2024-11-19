86fd4deec41acb247e817dce5b99a4cfd7ccc966
MJspace <mjsa02@kookmin.ac.kr>
2024-11-19 18:07:29 +0900

    refactor: 탭 및 공통서식 수정 삭제

--- src/pages/MyPage/index.tsx
+++ src/pages/MyPage/index.tsx
@@ -1,26 +1,10 @@
 import { useState } from 'react';
-import ProfilePage from '../ProfilePage/index';
 import Header from '../../components/Header/index';
 import Profile from '../../components/Profile/index';
 import Prediction from '../../components/Prediction';
-import Tab from '../../components/Tab';
 import * as m from './style';
 
-const HistoryPage = () => (
-  <m.HistoryContainer>
-    <m.PredictionContainer>
-      <Prediction />
-    </m.PredictionContainer>
-    <m.Graph />
-  </m.HistoryContainer>
-);
-
 const MyPage: React.FC = () => {
-  const [activeTab, setActiveTab] = useState<string>('히스토리 조회');
-
-  const handleTabChange = (tab: string) => {
-    setActiveTab(tab);
-  };
   return (
     <m.Container>
       <Header />
@@ -34,12 +18,13 @@ const MyPage: React.FC = () => {
             }}
           />
         </div>
-        <Tab
-          tabs={['히스토리 조회', '공통 서식 수정']}
-          onTabChange={handleTabChange}
-        />
-        {activeTab === '히스토리 조회' && <HistoryPage />}
-        {activeTab === '공통 서식 수정' && <ProfilePage />}
+        히스토리 조회
+        <m.HistoryContainer>
+          <m.PredictionContainer>
+            <Prediction />
+          </m.PredictionContainer>
+          <m.Graph />
+        </m.HistoryContainer>
       </m.InnerContainer>
     </m.Container>
   );

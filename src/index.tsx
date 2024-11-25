a782156b3f2a237d1afce494cdf1b90b3d552fe8
cg10036 <cg10036@gmail.com>
2024-11-25 18:12:29 +0900

    fix: disable strictmode

--- src/index.tsx
+++ src/index.tsx
@@ -9,10 +9,12 @@ const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement,
 );
 root.render(
-  <React.StrictMode>
+  // <React.StrictMode>
+  <>
     <GlobalStyle />
     <App />
-  </React.StrictMode>,
+  </>,
+  // </React.StrictMode>,
 );
 
 reportWebVitals();

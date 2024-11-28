d680a363e221f076e28a8e3df2531804463ec13b
MJspace <mjsa02@kookmin.ac.kr>
2024-11-29 03:33:57 +0900

    style:온보딩 페이지 디자인 수정

--- src/pages/OnboardingPage/OnboardingPage.tsx
+++ src/pages/OnboardingPage/OnboardingPage.tsx
@@ -5,16 +5,13 @@ import Header from '../../components/Header/index';
 const OnboardingPage: React.FC = () => {
   return (
     <>
-      {/* 배경 요소 */}
       <m.BackgroundGradient />
       <m.BackgroundCircles>
-        {/* 원 배경: 크기, 위치, 투명도를 다양화 */}
         <m.Circle size="200px" top="10%" left="20%" opacity={0.3} />
         <m.Circle size="150px" top="40%" left="75%" opacity={0.2} />
         <m.Circle size="100px" top="60%" left="30%" opacity={0.1} />
         <m.Circle size="250px" top="80%" left="15%" opacity={0.3} />
       </m.BackgroundCircles>
-      {/* 메인 콘텐츠 */}
       <m.Container>
         <Header />
         <m.ContentWrapper>

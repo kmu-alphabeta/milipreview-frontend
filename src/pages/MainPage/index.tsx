c16a616d6197ac107cd6ecfb03ce70e40885849b
MJspace <mjsa02@kookmin.ac.kr>
2024-11-29 04:43:23 +0900

    style: 주석제거

--- src/pages/MainPage/index.tsx
+++ src/pages/MainPage/index.tsx
@@ -8,7 +8,6 @@ import { getHistory } from '../../apis/history';
 import { useQuery } from 'react-query';
 import Spinner from '../../components/Spinner';
 import RecommendPrediction from '../../components/RecommendPrediction';
-import Group from '../../assets/Graph.svg';
 import RecommendGraph from '../../components/RecommendGraph';
 
 interface HistoryItem {
@@ -123,7 +122,6 @@ const MainPage: React.FC = () => {
               <br /> 맞춤 직종이에요!
             </m.Text>
             <m.Text style={{ color: 'red' }}>추천하는 군종 : {army}</m.Text>
-            {/* <img src={Group} alt="Group" /> */}
             <RecommendGraph />
             <m.Text>
               그중에서도 <br />

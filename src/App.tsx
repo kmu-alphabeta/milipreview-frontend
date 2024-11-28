72c0e42589b0d1c35ab120cbaeb5435b90ab8029
MJspace <mjsa02@kookmin.ac.kr>
2024-11-28 15:49:10 +0900

    style: 라우터 따로 뺌

--- src/App.tsx
+++ src/App.tsx
@@ -1,18 +1,8 @@
-import React from 'react';
-import { BrowserRouter, Route, Routes } from 'react-router-dom';
-import MainPage from './pages/MainPage';
-import OnboardingPage from './pages/OnboardingPage/OnboardingPage';
-import PredictionPage from './pages/PredictionPage/PredictionPage';
-import CommunityView from './components/Community/CommunityView';
-import CommunityWrite from './components/Community/CommunityWrite';
-import CommunityDetail from './components/Community/CommunityDetail';
 import GlobalStyle from './styles/GlobalStyles';
-import KakaoAuth from './components/KakaoAuth';
-import SearchPage from './pages/SearchPage';
-import PredictionResultPage from './pages/PredictionResultPage/PredictionResultPage';
+import { BrowserRouter } from 'react-router-dom';
 import { QueryClient, QueryClientProvider } from 'react-query';
+import AppRoutes from './AppRoutes';
 import './App.css';
-import MyPage from './pages/MyPage';
 
 const queryClient = new QueryClient();
 
@@ -20,18 +10,7 @@ const App: React.FC = () => {
   return (
     <QueryClientProvider client={queryClient}>
       <BrowserRouter>
-        <Routes>
-          <Route path="/" element={<MainPage />} />
-          <Route path="/onboarding" element={<OnboardingPage />} />
-          <Route path="/prediction" element={<PredictionPage />} />
-          <Route path="/mypage" element={<MyPage />} />
-          <Route path="/community" element={<CommunityView />} />
-          <Route path="/community/write" element={<CommunityWrite />} />
-          <Route path="/community/detail/:id" element={<CommunityDetail />} />
-          <Route path="/oauth" element={<KakaoAuth />} />
-          <Route path="/search" element={<SearchPage />} />
-          <Route path="/predictionresult/:id" element={<PredictionResultPage />} />
-        </Routes>
+        <AppRoutes />
         <GlobalStyle />
       </BrowserRouter>
     </QueryClientProvider>
